885411c73f7b80b826d8fdd269b3989e39b32c94
Igors78 <oleinikovsigors@gmail.com>
2020-08-18 10:16:07 +0300

    Input scripting

--- js/script.js
+++ js/script.js
@@ -5,15 +5,41 @@ const enter = document.querySelector('#enter');
 const period = document.querySelector('#period');
 const numbers = Array.from(document.querySelectorAll('.number'));
 const operators = Array.from(document.querySelectorAll('.operator'));
+let displayValue = '';
 console.log(numbers);
 console.log(operators);
 
-numbers.forEach(number => number.addEventListener('click', printToDisplay));
-operators.forEach(operator => operator.addEventListener('click', addOperator));
-del.addEventListener('click', removeFromDisplay);
-enter.addEventListener('click', calculate);
-reset.addEventListener('click', clearDisplay);
+numbers.forEach(number => number.addEventListener('click', updateDisplay));
+//operators.forEach(operator => operator.addEventListener('click', addOperator));
+//del.addEventListener('click', removeFromDisplay);
+//enter.addEventListener('click', calculate);
+//reset.addEventListener('click', clearDisplay);
 period.addEventListener('click', addPeriod);
+
+function updateDisplay(e) {
+    console.log(e.target.textContent);
+    if (display.textContent.length == 16) {
+        return;
+    }else if (display.textContent === '0' && e.target.textContent == 0){
+        return;
+    }else{
+        displayValue == ''? displayValue = e.target.textContent : displayValue += e.target.textContent;
+        display.textContent = displayValue;
+        console.log(displayValue);
+}
+}
+function addPeriod(e) {
+    if (display.textContent.indexOf('.') > 0){
+        return;
+    }
+    displayValue == ''? displayValue = '0' + e.target.textContent : displayValue += e.target.textContent;
+    display.textContent = displayValue;
+    console.log(displayValue);
+}
+
+//function addOperator(e) {
+
+//}
 
 
 
